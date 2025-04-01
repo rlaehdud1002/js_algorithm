137c94069061370615ebf60a1878f031f27a5dda
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2025-04-01 12:41:11 +0900

    0401/이명욱/chapter2

---
+++ week_6/이명욱/chapter2/lesson1.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let n = Number(input[0]);
+// Please Write your code here.
+
+function print(n){
+  if(n === 0) return;
+  for(let i = 0; i < n; i++){
+    process.stdout.write('* ');
+  }
+  console.log();
+  print(n - 1);
+  for(let i = 0; i < n; i++){
+    process.stdout.write('* ');
+  }
+  console.log();
+}
+
+print(n);
